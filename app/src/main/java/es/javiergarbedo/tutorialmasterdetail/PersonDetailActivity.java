2f6615109f0647135c93dbd428552a482f7d3012
Javier García <clases@javiergarbedo.es>
2014-11-10 09:59:27 +0100

    Detalle con páginas de desplazamiento lateral Swipe Views

--- app/src/main/java/es/javiergarbedo/tutorialmasterdetail/PersonDetailActivity.java
+++ app/src/main/java/es/javiergarbedo/tutorialmasterdetail/PersonDetailActivity.java
@@ -2,67 +2,136 @@ package es.javiergarbedo.tutorialmasterdetail;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
+/*
+Modificaciones realizadas en la Activity encargada de mostrar el detalle en una aplicación de tipo
+master-detail para que se puedan utilizar páginas de desplazamiento lateral o pestañas.
 
-/**
- * An activity representing a single Person detail screen. This
- * activity is only used on handset devices. On tablet-size devices,
- * item details are presented side-by-side with a list of items
- * in a {@link PersonListActivity}.
- * <p>
- * This activity is mostly just a 'shell' activity containing nothing
- * more than a {@link PersonDetailFragment}.
+Más información sobre la distribución en páginas laterales (Swipe Views) o pestañas (Tabs) en:
+http://developer.android.com/training/implementing-navigation/lateral.html
  */
+
 public class PersonDetailActivity extends ActionBarActivity {
+
+    /* TODO: Añadir las 2 líneas siguientes
+    La clase SectionsPagerAdapter no se encontrará inicialmente, porque se va a crear después
+    dentro de esta misma clase.
+     */
+    SectionsPagerAdapter mSectionsPagerAdapter;
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_detail);
 
-        // Show the Up button in the action bar.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // savedInstanceState is non-null when there is fragment state
-        // saved from previous configurations of this activity
-        // (e.g. when rotating the screen from portrait to landscape).
-        // In this case, the fragment will automatically be re-added
-        // to its container so we don't need to manually add it.
-        // For more information, see the Fragments API guide at:
-        //
-        // http://developer.android.com/guide/components/fragments.html
-        //
-        if (savedInstanceState == null) {
-            // Create the detail fragment and add it to the activity
-            // using a fragment transaction.
-            Bundle arguments = new Bundle();
-            arguments.putString(PersonDetailFragment.ARG_ITEM_ID,
-                    getIntent().getStringExtra(PersonDetailFragment.ARG_ITEM_ID));
-            PersonDetailFragment fragment = new PersonDetailFragment();
-            fragment.setArguments(arguments);
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.person_detail_container, fragment)
-                    .commit();
-        }
+        //TODO: Añadir estas líneas cambiando el ID del Layout al correspondiente a
+        // la Activity de detalle. En ese layout debe cambiarse el elemento raíz,
+        // en la vista XML, cambiando FrameLayout por android.support.v4.view.ViewPager
+        // Lo que hacen estas líneas es adaptar el layout de la Activity a un objeto
+        // de tipo ViewPager para que se pueda mostrar la Activity por páginas o pestañas
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.person_detail_container); //Cambiar
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        //TODO: Eliminar todo este bloque que se colocará parecido posteriormente
+//        if (savedInstanceState == null) {
+//            // Create the detail fragment and add it to the activity
+//            // using a fragment transaction.
+//            Bundle arguments = new Bundle();
+//            arguments.putString(PersonDetailFragment.ARG_ITEM_ID,
+//                    getIntent().getStringExtra(PersonDetailFragment.ARG_ITEM_ID));
+//            PersonDetailFragment fragment = new PersonDetailFragment();
+//            fragment.setArguments(arguments);
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.person_detail_container, fragment)
+//                    .commit();
+//        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. Use NavUtils to allow users
-            // to navigate up one level in the application structure. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
             NavUtils.navigateUpTo(this, new Intent(this, PersonListActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    /* TODO: Copiar toda esta clase, que se utiliza para poder crear objetos que permitan adaptar
+    varios Fragment que se incluyan en una Activity de modo que se puedan mostrar a modo de páginas
+    o pestañas.
+    Observa que esta clase SectionsPagerAdapter se encuentra dentro de la anterior xxxDetailActivity
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /*
+        Retorna el Fragment que corresponda en función de la posición (position) que corresponda,
+        que es la página o pestaña que deberá mostrarse dentro de la Activity
+         */
+        @Override
+        public Fragment getItem(int position) {
+            //TODO: Meter aquí el siguiente código que es similar al que había en el método
+            // onCreate original de esta Activity
+            Bundle arguments = new Bundle();
+            //Se pasa al fragment el id de la posición seleccionada por el usuario en la lista
+            arguments.putString(PersonDetailFragment.ARG_ITEM_ID,
+                    getIntent().getStringExtra(PersonDetailFragment.ARG_ITEM_ID));
+            switch(position) {
+                case 0:
+                    //Se indica el Fragment que se cargará para la position 0
+                    PersonDetailFragment fragment1 = new PersonDetailFragment();
+                    fragment1.setArguments(arguments);
+                    return fragment1;
+                case 1:
+                    //Se indica el Fragment que se cargará para la position 1
+                    //TODO: Hay que crear una clase diferente para cada contenido de las pestañas
+                    PersonDetailFragment2 fragment2 = new PersonDetailFragment2();
+                    fragment2.setArguments(arguments);
+                    return fragment2;
+            }
+            return null;
+        }
+
+        /*
+        Retorna el número de páginas o pestañas (Fragments) que se incluirán en la Activity.
+        Debe coincidir con el número de casos incluidos en el método getItem anterior.
+         */
+        @Override
+        public int getCount() {
+            // TODO: Indicar aquí el número de pestañas que se van a tener
+            return 2;
+        }
+
+        /*
+        Retorna el título que se mostrará en cada página o pestaña
+         */
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    //TODO: Crear un recurso String para cada título de las pestañas
+                    return getString(R.string.title_section1);
+                case 1:
+                    return getString(R.string.title_section2);
+            }
+            return null;
+        }
+    }
+
 }
